ebbb11156aa06ff8b618b317a17e6bf775af35f9
dav1app <davi.mello.28@gmail.com>
2021-08-17 21:32:51 -0300

    feat: add intensity to red light

--- src/3d/components/light/RedLight.js
+++ src/3d/components/light/RedLight.js
@@ -1,8 +1,8 @@
 import { PointLight as _PointLight } from 'three'
 
 export class RedLight {
-  constructor ({ x = 0, y = 1, z = 1 }) {
-    const light = new _PointLight(0xff0000, 10)
+  constructor ({ x = 0, y = 1, z = 1, intensity = 10 }) {
+    const light = new _PointLight(0xff0000, intensity)
     light.castShadow = true
     light.shadow.mapSize.width = 512 // default
     light.shadow.mapSize.height = 512 // default
